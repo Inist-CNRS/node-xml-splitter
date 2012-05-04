ae2c654636c257f25fb703e5b2aecdf4362c2ecd
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-05-04 17:11:28 +0200

    for operators [ci skip]

---
+++ test/operators.js
@@ -0,0 +1,61 @@
+'use strict';
+var XMLSplitter = require('../')
+
+exports.setUp = function (callback) {
+	callback()
+}
+
+
+exports.t01 = function (test) {
+  var xs = new XMLSplitter('//record')
+  xs.on('data', function (node) {
+      test.equal(node['$t'], '1')
+    }
+  )
+  xs.on('end', function (c) {
+      test.equal(c, 1)
+      test.done()
+    }
+  )
+  xs.parseString('<Record>1</Record>')
+}
+
+exports.t02 = function (test) {
+  var i = 0, xs = new XMLSplitter('//item')
+  xs.on('data', function (node) {
+      i++
+      if (i === 1) {
+        test.equal(node['$t'], '1')
+      }
+      else if (i == 2) {
+        test.equal(node['$t'], '2')
+      }
+    }
+  )
+  xs.on('end', function (c) {
+      test.equal(c, 2)
+      test.done()
+    }
+  )
+  xs.parseString('<record><item>1</item><item>2</item></record>')
+}
+
+exports.t03 = function (test) {
+  var i = 0, xs = new XMLSplitter('//item')
+  xs.on('data', function (node) {
+      i++
+      if (i === 1) {
+        test.equal(node['value'], '1')
+      }
+      else if (i == 2) {
+        test.equal(node['value'], '2')
+      }
+    }
+  )
+  xs.on('end', function (c) {
+      test.equal(c, 2)
+      test.done()
+    }
+  )
+  xs.parseString('<record><item value="1"/><item value="2"><record><item value="3"/></record></item></record>')
+}
