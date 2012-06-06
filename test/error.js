2ef667e66092d802c1330700e324f7f768e13f63
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-06-06 11:32:15 +0200

    "end" emit even if there are errors

---
+++ test/error.js
@@ -0,0 +1,32 @@
+var XMLSplitter = require('../')
+
+exports.setUp = function (callback) {
+  callback()
+}
+
+
+exports.t01 = function (test) {
+  var xs = new XMLSplitter('/record/item')
+  var er = false
+  xs.on('error', function (e) {
+      er = true
+    }
+  )
+  xs.on('end', function (c) {
+      test.equal(er , true)
+      test.equal(c , 2)
+      test.done()
+  })
+  xs.parseString('<Record><item>1</item><item>2</item><item</Record>')
+}
+
+exports.t02 = function (test) {
+  var xs = new XMLSplitter('//item')
+  xs.on('end', function (c) {
+      test.equal(c , 2)
+      test.done()
+  })
+  xs.parseString('<Record><item>1</item><item>2</item>')
+}
+/* */
+
